e19d3430a69e73de67a17f94ad4befc234ff99c0
Me <me@victor-hungs-macbook-pro.local>
2011-08-10 16:37:59 -0700

    new missions model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Flamingle::Application.routes.draw do
+  resources :missions
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20110810231815_create_missions.rb
@@ -0,0 +1,15 @@
+class CreateMissions < ActiveRecord::Migration
+  def self.up
+    create_table :missions do |t|
+      t.string :name
+      t.string :description
+      t.string :type
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :missions
+  end
+end
