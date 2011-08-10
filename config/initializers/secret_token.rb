d061780868d22b2258b1ede281133e4cf16c3f81
Vincent Siao <vincentsiao@gmail.com>
2011-08-09 22:50:07 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flamingle::Application.config.secret_token = '4efaa9318c5ca8049715fe9d416c5bb5e13f3250db63dbb0bc5e7991d43d47e1cd55bf42a1b168e7cd85526f2df0dfe7ee88636713a929192f28dd74f9acffe8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Flamingle::Application.config.session_store :cookie_store, :key => '_flamingle_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Flamingle::Application.config.session_store :active_record_store
