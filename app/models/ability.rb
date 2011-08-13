768441c6faa27239e4ae90a486b78fa3c91eab44
Vincent Siao <vincentsiao@gmail.com>
2011-08-13 12:53:50 -0700

    cancan ability to manage own missions fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,10 +10,9 @@ class Ability
       can :manage, Mission
     else
       can [:read, :create, :accept], Mission
-      can [:update, :destroy], Mission, :user_id => user.id
+      can :manage, Mission, :user_id => user.id
     end
     
-    #cannot :accept, Mission, :user_id => user.id
     cannot :accept, Mission do |mission|
       (mission.attempting_users.exists? user) || (mission.user == user)
     end
