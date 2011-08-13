f92ca307fa5d2376e878cf06a110086478377d82
Victor Hung <victorhung92@gmail.com>
2011-08-12 23:56:40 -0700

    mission model short_description

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -2,4 +2,12 @@ class Mission < ActiveRecord::Base
   belongs_to :user
   belongs_to :priority
   has_many :mission_attempts
+
+  def short_description
+    if self.description.size > 100
+  	  self.description[0 .. 100] + '...'
+  	else 
+  	  self.description
+  	end
+  end
 end
