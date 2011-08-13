4901fcd2a8d3f9105e7b1ab6259f57c347704c01
Vincent Siao <vincentsiao@gmail.com>
2011-08-13 00:28:13 -0700

    nil description for short_description method fix

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -5,10 +5,12 @@ class Mission < ActiveRecord::Base
   has_many :attempting_users, :through => :mission_attempts, :source => :user
 
   def short_description
-    if self.description.size > 100
-  	  self.description[0 .. 100] + '...'
-  	else 
-  	  self.description
-  	end
+    if self.description
+      if self.description.size > 100
+  	    self.description[0 .. 100] + '...'
+  	  else 
+  	    self.description
+  	  end
+    end
   end
 end
