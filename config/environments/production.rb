8d52cd0176baf6d7a10d84f2f94a981e0e699a90
Vincent Siao <vincentsiao@gmail.com>
2011-08-14 00:03:50 -0700

    production action_mailer url changed to flamingle.heroku.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,7 @@ Flamingle::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'flamingle.heroku.com' }
 
   # Enable threaded mode
   # config.threadsafe!
