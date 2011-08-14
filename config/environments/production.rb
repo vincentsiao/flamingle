ded94da5f38a0a61a63ddc3a76e92a001b3d84aa
Vincent Siao <vincentsiao@gmail.com>
2011-08-14 00:18:28 -0700

    add sendgrid smtp server settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,16 @@ Flamingle::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'flamingle.heroku.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'heroku.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address    => 'smtp.sendgrid.net',
+    :port       => '25',
+    :authentication => :plan,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 
   # Enable threaded mode
   # config.threadsafe!
